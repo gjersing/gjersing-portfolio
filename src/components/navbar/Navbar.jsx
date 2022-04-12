e9d483a87b47c88e5fdd96711258299a2c06ef16
Christopher Gjersing <csgjersing@gmail.com>
2022-04-12 11:47:03 -0700

    Implement navbar with smooth scroll to different sections

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './navbar.css'
+import {FiHome} from 'react-icons/fi'
+import {BiUser, BiMessageDetail, BiBook} from 'react-icons/bi'
+import {RiComputerLine} from 'react-icons/ri'
+import {useState} from 'react'
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState('#');
+  const sections = document.querySelectorAll("section");
+
+  window.onscroll = () => {
+    var current = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      if (window.pageYOffset >= sectionTop-150) {
+        current = section.getAttribute("id"); }
+    });
+    setActiveNav('#' + current);
+  };
+
   return(
-    <div>Navbar</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FiHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#portfolio"  onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiComputerLine /></a>
+      <a href="#contact"  onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail /></a>
+    </nav>
   );
 }
 
