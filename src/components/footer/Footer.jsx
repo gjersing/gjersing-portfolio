fe07205f1a9fe857279a7cf3fbc9f24627a8a98e
Christopher Gjersing <csgjersing@gmail.com>
2022-04-12 18:11:01 -0700

    Implement footer and favicon

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
 
 const Footer = () => {
   return(
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>GJERSING</a>
+
+      <div className='footer__socials'>
+        <a href='https://www.linkedin.com/in/gjersing/' target="_blank"><BsLinkedin /></a>
+        <a href='https://github.com/gjersing' target="_blank"><FaGithub /></a>
+        <a href='https://twitter.com/csgjersing' target="_blank"><FaTwitter /></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small className='text-bright'>&copy; Christopher Gjersing 2022</small>
+      </div>
+    </footer>
   );
 }
 
