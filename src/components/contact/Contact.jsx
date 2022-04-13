c75b3429e7bb6bb3830633c298cbd53b74f9e027
Christopher Gjersing <csgjersing@gmail.com>
2022-04-12 17:11:49 -0700

    Implement contact form with emailjs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com'
 import './contact.css'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_5w1q4of', 'template_kvi7lod', form.current, '4eNqrj6cVupulf9a1');
+    e.target.reset();
+  };
+
   return(
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h2>Contact</h2>
+
+      <div className='container contact__container'>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type='text' name='name' placeholder='Full Name' required />
+          <input type='email' name='email' placeholder='Email' required />
+          <textarea name='message' rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary '>Send Message</button>
+        </form>
+      </div>
+    </section>
   );
 }
 
